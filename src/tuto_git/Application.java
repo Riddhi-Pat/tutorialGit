d96c710c87b07c5aea82825164fc1d8bbd07197f
Riddhi-Pat <patelridhi96@gmail.com>
2019-10-02 12:38:21 +0200

    Changes the project

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -27,7 +27,7 @@ public class Application extends Frame implements WindowListener {
 		this.add(view);
 		
 		this.pack();
-		this.setTitle("The clic game begin");
+		this.setTitle("The clic game over");
 		this.setVisible(true);	
 	}
 
